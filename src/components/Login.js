e9a13108071c6cd88fa701d9f8b096316ef854cc
Marco-Castro01 <castroo8a2@gmail.com>
2024-10-14 03:00:44 -0400

    arreglo

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 // components/Login.js
 import React, { useState, useEffect } from 'react';
-import { Link,  useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {
   MDBContainer,
   MDBCol,
@@ -11,8 +11,6 @@ import {
   MDBCardImage,
 } from 'mdb-react-ui-kit';
 
-
-
 const Login = ({ onLogin, isAuthenticated }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -21,6 +19,12 @@ const Login = ({ onLogin, isAuthenticated }) => {
   const navigate = useNavigate();
 
   const handleLogin = async () => {
+    // Validar que los campos no estén vacíos
+    if (!username || !password) {
+      setMessage('Por favor, completa todos los campos.');
+      return; // No enviar la solicitud si hay campos vacíos
+    }
+
     try {
       const response = await fetch('http://ec2-3-141-10-4.us-east-2.compute.amazonaws.com:4000/login', {
         method: 'POST',
@@ -39,7 +43,7 @@ const Login = ({ onLogin, isAuthenticated }) => {
         // Introduce a delay before redirecting to the home page
         setTimeout(() => {
           navigate('/');
-        }, 4000); // 4000 milliseconds (5 seconds)
+        }, 4000); // 4000 milliseconds (4 seconds)
       } else {
         // Login failed
         setMessage(data.message);
@@ -55,7 +59,7 @@ const Login = ({ onLogin, isAuthenticated }) => {
       // Introduce a delay before redirecting to the home page
       const delay = setTimeout(() => {
         navigate('/');
-      }, 4000); // 4000 milliseconds (5 seconds)
+      }, 4000); // 4000 milliseconds (4 seconds)
 
       // Clear the timeout if the component is unmounted
       return () => clearTimeout(delay);
@@ -63,101 +67,101 @@ const Login = ({ onLogin, isAuthenticated }) => {
   }, [isAuthenticated, navigate]);
 
   return (
-    <MDBContainer className="mt-5" fluid>
-      <MDBRow className="justify-content-center">
-        <MDBCol lg="8">
-          <MDBCard>
-            <MDBRow className="m-0">
-              <MDBCol md="6">
-                <MDBCardImage
-                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
-                  alt="image"
-                  fluid
-                />
-              </MDBCol>
-              <MDBCol md="6">
-                <MDBCardBody>
-                  <MDBRow className="mb-4">
-                    <MDBCol className="text-center">
-                      <h2>Login</h2>
-                    </MDBCol>
-                  </MDBRow>
-
-                  <MDBRow className="mb-4">
-                    <MDBCol>
-                      <label className="font-label">Username</label>
-                      <MDBInput
-                        id="formControlLg"
-                        type="text"
-                        size="sm"  // Change size to "sm" for smaller input
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                      />
-                    </MDBCol>
-                  </MDBRow>
-
-                  <MDBRow className="mb-4">
-                    <MDBCol>
-                      <label className="font-label">Password</label>
-                      <MDBInput
-                        id="formControlLg"
-                        type="password"
-                        size="sm"  // Change size to "sm" for smaller input
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                      />
-                    </MDBCol>
-                  </MDBRow>
-
-                  <MDBRow className="mb-4 justify-content-center">
-                    <MDBCol>
-                      <button
-                        className="btn btn-primary btn-lg"
-                        onClick={handleLogin}
-                        style={{ width: '100%' }}
-                      >
-                        Sign in
-                      </button>
-                    </MDBCol>
-                  </MDBRow>
-
-                  <MDBRow className="mb-4">
-                    <MDBCol>
-                      <div className="divider d-flex align-items-center">
-                        <p className="text-center fw-bold text-black mb-0">OR</p>
-                      </div>
-                    </MDBCol>
-                  </MDBRow>
-
-                  <MDBRow className="mb-4">
-                    <MDBCol className="text-center">
-                      <p className="mb-0 text-black">Don't have an account?</p>
-                      <Link to="/register" className="fw-bold">Register here</Link>
-                    </MDBCol>
-                  </MDBRow>
-
-                  {message && (
-                    <MDBRow>
-                      <MDBCol>
-                        <p className="text-center text-danger">{message}</p>
+      <MDBContainer className="mt-5" fluid>
+        <MDBRow className="justify-content-center">
+          <MDBCol lg="8">
+            <MDBCard>
+              <MDBRow className="m-0">
+                <MDBCol md="6">
+                  <MDBCardImage
+                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+                      alt="image"
+                      fluid
+                  />
+                </MDBCol>
+                <MDBCol md="6">
+                  <MDBCardBody>
+                    <MDBRow className="mb-4">
+                      <MDBCol className="text-center">
+                        <h2>Login</h2>
                       </MDBCol>
                     </MDBRow>
-                  )}
 
-                  {redirecting && (
-                    <MDBRow>
+                    <MDBRow className="mb-4">
                       <MDBCol>
-                        <p className="text-center text-success">Logging in. Redirecting to the home page...</p>
+                        <label className="font-label">Username</label>
+                        <MDBInput
+                            id="formControlLg"
+                            type="text"
+                            size="sm"  // Change size to "sm" for smaller input
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
                       </MDBCol>
                     </MDBRow>
-                  )}
-                </MDBCardBody>
-              </MDBCol>
-            </MDBRow>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+
+                    <MDBRow className="mb-4">
+                      <MDBCol>
+                        <label className="font-label">Password</label>
+                        <MDBInput
+                            id="formControlLg"
+                            type="password"
+                            size="sm"  // Change size to "sm" for smaller input
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                      </MDBCol>
+                    </MDBRow>
+
+                    <MDBRow className="mb-4 justify-content-center">
+                      <MDBCol>
+                        <button
+                            className="btn btn-primary btn-lg"
+                            onClick={handleLogin}
+                            style={{ width: '100%' }}
+                        >
+                          Sign in
+                        </button>
+                      </MDBCol>
+                    </MDBRow>
+
+                    <MDBRow className="mb-4">
+                      <MDBCol>
+                        <div className="divider d-flex align-items-center">
+                          <p className="text-center fw-bold text-black mb-0">OR</p>
+                        </div>
+                      </MDBCol>
+                    </MDBRow>
+
+                    <MDBRow className="mb-4">
+                      <MDBCol className="text-center">
+                        <p className="mb-0 text-black">Don't have an account?</p>
+                        <Link to="/register" className="fw-bold">Register here</Link>
+                      </MDBCol>
+                    </MDBRow>
+
+                    {message && (
+                        <MDBRow>
+                          <MDBCol>
+                            <p className="text-center text-danger">{message}</p>
+                          </MDBCol>
+                        </MDBRow>
+                    )}
+
+                    {redirecting && (
+                        <MDBRow>
+                          <MDBCol>
+                            <p className="text-center text-success">Logging in. Redirecting to the home page...</p>
+                          </MDBCol>
+                        </MDBRow>
+                    )}
+                  </MDBCardBody>
+                </MDBCol>
+              </MDBRow>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
   );
 };
 
